e8fd0a370ebd2a61fcc86adee649d81a67cdffc8
meary2018 <yhattori2018@gmail.com>
2022-02-27 08:34:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -30,36 +30,45 @@ def say_hello():
     return "Hello"
 
 
-##########ここから↓LINEBOTの存在を知らしめる処理####################
+## 1 ##
+#Webhookからのリクエストをチェックします。
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
+    # リクエストヘッダーから署名検証のための値を取得します。
     signature = request.headers['X-Line-Signature']
-
-    # get request body as text
+ 
+    # リクエストボディを取得します。
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
+ 
     # handle webhook body
+    # 署名を検証し、問題なければhandleに定義されている関数を呼び出す。
     try:
         handler.handle(body, signature)
+        #署名検証で失敗した場合、例外を出す。
     except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
-
+        # handleの処理を終えればOK
     return 'OK'
-##########ここまで↑LINEBOTの存在を知らしめる処理####################
-
-
+ 
+## 2 ##
+###############################################
+#LINEのメッセージの取得と返信内容の設定(オウム返し)
+###############################################
+ 
+#LINEでMessageEvent（普通のメッセージを送信された場合）が起こった場合に、
+#def以下の関数を実行します。
+#reply_messageの第一引数のevent.reply_tokenは、イベントの応答に用いるトークンです。 
+#第二引数には、linebot.modelsに定義されている返信用のTextSendMessageオブジェクトを渡しています。
+ 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
-    print("DONE")
-
-
+        TextSendMessage(text=event.message.text)) #ここでオウム返しのメッセージを返します。
+ 
+# ポート番号の設定
 if __name__ == "__main__":
-    app.run()
-
-
+#    app.run()
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
