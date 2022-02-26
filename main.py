8e75828e6ee69667e5e4e659fcb96b7c616b9f97
meary2018 <yhattori2018@gmail.com>
2022-02-26 22:21:37 +0900

    動作確認

--- main.py
+++ main.py
@@ -56,6 +56,7 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
+    print("DONE")
 
 
 if __name__ == "__main__":
